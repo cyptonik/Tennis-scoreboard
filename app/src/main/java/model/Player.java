84bae13253a9659b0237eb41fd7406f32987e571
Cypopik <enixydex15@gmail.com>
2026-04-04 20:10:01 +0300

    Added models Match and Player and integrated hibernate annotaions

--- app/src/main/java/model/Player.java
+++ app/src/main/java/model/Player.java
@@ -1,4 +1,30 @@
 package model;
 
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "Players")
 public class Player {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    public Player(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Player() { }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
