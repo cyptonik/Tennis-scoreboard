8f1beacb744a6a46211dcb8aac4cfd29e67e1be3
Cypopik <enixydex15@gmail.com>
2026-04-04 20:50:50 +0300

    Removed unnecessary files

--- deploy.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-export SUDO_ASKPASS=/usr/lib/ssh/x11-ssh-askpass
-
-# sudo -A rm -rf /var/lib/tomcat10/webapps/app.war
-sudo -A cp ./build/libs/app.war /var/lib/tomcat10/webapps/tennis_app.war
-
