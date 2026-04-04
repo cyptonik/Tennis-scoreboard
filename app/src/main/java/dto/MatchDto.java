e1f65d992e42ed2ac76c0830168f9ca0e7c07870
Cypopik <enixydex15@gmail.com>
2026-04-04 20:25:24 +0300

    Create all necessary DTOs for MatchService

---
+++ app/src/main/java/dto/MatchDto.java
@@ -0,0 +1,31 @@
+package dto;
+
+public class MatchDto {
+    private final Long id;
+    private final PlayerDto player1;
+    private final PlayerDto player2;
+    private final PlayerDto winner;
+
+    public MatchDto(Long id, PlayerDto player1, PlayerDto player2, PlayerDto winner) {
+        this.id = id;
+        this.player1 = player1;
+        this.player2 = player2;
+        this.winner = winner;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public PlayerDto getPlayer1() {
+        return player1;
+    }
+
+    public PlayerDto getPlayer2() {
+        return player2;
+    }
+
+    public PlayerDto getWinner() {
+        return winner;
+    }
+}
