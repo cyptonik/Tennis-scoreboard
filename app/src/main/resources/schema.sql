7844c589fee6e9d34f5dec66f48a20479352a20f
Cypopik <enixydex15@gmail.com>
2026-04-04 20:42:43 +0300

    Created schema.sql for initializing database tables

--- app/src/main/resources/schema.sql
+++ app/src/main/resources/schema.sql
@@ -5,9 +5,15 @@ CREATE TABLE IF NOT EXISTS players (
 
 CREATE TABLE IF NOT EXISTS matches (
     id        INT AUTO_INCREMENT PRIMARY KEY,
-    player1   INT NOT NULL,
-    player2   INT NOT NULL,
-    winner    INT,
-    FOREIGN KEY (player1) REFERENCES players(id),
-    FOREIGN KEY (player2) REFERENCES players(id)
+    player1_id   INT NOT NULL,
+    player2_id   INT NOT NULL,
+    winner_id    INT,
+    FOREIGN KEY (player1_id) REFERENCES players(id),
+    FOREIGN KEY (player2_id) REFERENCES players(id)
 );
+
+INSERT INTO Players (name) VALUES ('Alice');
+INSERT INTO Players (name) VALUES ('Andrew');
+INSERT INTO Players (name) VALUES ('Johnny');
+INSERT INTO Players (name) VALUES ('Olya');
+INSERT INTO Players (name) VALUES ('Bob');
